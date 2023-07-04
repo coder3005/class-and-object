eacfba1e010ec2c5a739607455147aeb52813179
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-04 16:36:21 +0530

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,18 @@
+/* Class & Object */
+class demo
+{
+    int a=10; String b="abhi";
+    void show()
+    {
+        System.out.print(a+" "+b);
+    }
+}
+class test
+{
+    public static void main(String[] args) {
+       
+        demo  r=new demo();
+        r.show();
+    }
+    
+}
