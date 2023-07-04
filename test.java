03f9906706685f2757b9868b7e38e569c905a187
coder3005 <138576028+coder3005@users.noreply.github.com>
2023-07-04 15:27:18 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,19 @@
+/* Class & Object */
+class demo
+{
+    int a=10; String b="abhi";
+    void show()
+    {
+        System.out.print(a+" "+b);
+    }
+}
+class test
+{
+    public static void main(String[] args) {
+       
+        demo r;
+        r=new demo();
+        r.show();
+    }
+    
+}
